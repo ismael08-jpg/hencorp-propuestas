19948395c01a2806abfaea71c39eace7e9681c0d
Ismael Castillo <ismael.castillo19@itca.edu.sv>
2021-02-27 12:47:51 -0600

    Agregar busquedas catalogo

--- resources/views/catalogo/catalogoCreditos.blade.php
+++ resources/views/catalogo/catalogoCreditos.blade.php
@@ -2,38 +2,74 @@
 
 @section('title','Catalogo de Créditos')
 
+
 @section('content')
-    {{-- <h3>Aquí se muestran los creditos</h3>
+
+    <div class="row">
+        <div class="col-12">
+            <br>
+        </div>
+    </div>
+    <div class="row">
+            <div style="background-color: white" class="ml-md-5 col-xs-12 table-responsive table-responsive  col-md-9  rounded-lg">
+                <form action="{{route('catalogo-creditos.index')}}" method="GET">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+                            <label for="">Monto disponible mayor a</label>
+                            <input type="number" name="mayor_a" class="numero">
+                        </div>
+                        <div class="col-md-6 col-sm-12">
+                            <label for="">menor a</label>
+                            <input type="number" name="menor_a" class="numero">
+                        </div>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-md-5"></div>
+                        <div class="col-md-2">
+                            <center>
+                                <button class="filtrar">filtrar</button>
+                            </center>
+                        </div>
+                        <div class="col-md-5"></div>
+                    </div>
+                </form>
+                <br>
+                <table with="100%" class=" w-100 table-hover tabla">
+                    <thead class="">
+                            <tr>
+                            <th scope="col">Deudor</th>
+                            <th scope="col">Monto disponible hasta por</th>
+                            <th scope="col">Tasa %</th>
+                            <th scope="col">Vencimiento</th>
+                            <th>Grupo/país</th>
+                            </tr>
+                    </thead>
+                        
+                    <tbody>
+                            @foreach ($catalogo as $catalogos)
+                            <tr>
+                                <td scope="row">{{$catalogos->nombre_deudor}}</td>
+                                <td>{{$catalogos->NLP}}</td>
+                                <td>{{$catalogos->costo_ponderado}}</td>
+                                <td>{{$catalogos->fecha_vencimiento}}</td>
+                                <td>{{$catalogos->grupo_economico}}</td>
+                            </tr>
+                            @endforeach
+                    </tbody>
+                </table>
+                <br>
+                {{$catalogo->links()}}
+                
+                <br>
+                <br>
+            </div>
+            <div class="col-md-2 col-xs-12 bg-naranja">
+                <div class="menu-1">
+
+                </div>
+            </div>
+    </div>    
+
     
-    <ul>
-        @foreach ($catalogo as $catalogos)
-            <li>
-                {{$catalogos->nombre_deudor}}
-            </li>
-        @endforeach
-    </ul>
-
-    {{$catalogo->links()}} --}}
-
-    <div class="grid grid-cols-12 mt-20">
-        <div class="bg-green-300 text-center rounded-full col-start-2 col-span-3"><p class="mt-4">Deudor</p></div>
-        <div class="bg-green-300 text-center rounded-full">Monto disponible hasta por</div>
-        <div class="bg-green-300 text-center rounded-full"><p class="mt-4">Tasa %</p></div>
-        <div class="bg-green-300 text-center rounded-full"><p class="mt-4">Vencimiento</p></div>
-        <div class="bg-green-300 text-center rounded-full col-span-2"><p class="mt-4">Grupo/pais</p></div>
-        <div class="bg-green-300 text-center rounded-full col-span-2"><p class="mt-4">Giro de negocio</p></div>
-    </div>
-    @foreach ($catalogo as $catalogos)
-        
-    <div class="grid grid-cols-12 mt-1">
-        <div class="bg-gray-200 text-center rounded-full col-start-2 col-span-3">{{$catalogos->nombre_deudor}}</div>
-        <div class="bg-gray-200 text-center rounded-full">{{$catalogos->NLP}}</div>
-        <div class="bg-gray-200 text-center rounded-full">{{$catalogos->costo_ponderado}}</div>
-        <div class="bg-gray-200 text-center rounded-full">{{$catalogos->fecha_vencimiento}}</div>
-        <div class="bg-gray-200 text-center rounded-full col-span-2">{{$catalogos->grupo_economico}}</div>
-        <div class="bg-gray-200 text-center rounded-full col-span-2">Giro de negocio</div>
-    </div>
-        
-    @endforeach
-    {{ $catalogo->links() }}
 @endsection
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar row navbar-white bg-azul">
+    
+        <div class="col-md-4">
+            
+        </div>
+        <div class="col-md-4">
+            <center>
+                <a class="navbar-brand" href="#">
+                    <img src="{{asset('assets/img/hencorp.jpeg')}}" width="200" height=""  #02163a" class=" d-inline-block align-top" alt="">
+                  </a>
+            </center>
+        </div>
+        <div class="col-md-4">
+
+        </div>
+    
+</nav>
