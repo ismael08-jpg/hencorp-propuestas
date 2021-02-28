cadf60c7d7ca1e48bf749bc2bcda10c56476fb1c
Roberto Carlos López Abarca <rcla200031@gmail.com>
2021-02-27 19:08:33 -0600

    Trabajando en vista de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,27 @@
-@extends('layouts.app')
+@extends('layouts.master')
+
+@section('title', 'Inicio de Sesión')
+
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+    <div class="container-login">
+        <div class="d-flex w-100 justify-content-center">
+            <form action="" class="p-4 frm-login">
+                <div class="form-group">
+                    <label for="usuario">Nombre de usuario</label>
+                    <input type="text" name="usuario" id="usuario" class="form-control" value="{{ old('usuario') }}"
+                        autofocus required>
                 </div>
-            </div>
+                <div class="form-group">
+                    <label for="password">Contraseña</label>
+                    <div class="position-r">
+                        <input type="password" id="password" name="password" class="form-control" required>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <input type="submit" value="Acceder" class="btn btn-success">
+                </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,17 +1,14 @@
-<nav class="navbar row navbar-white bg-azul">
-    
-        <div class="col-md-4">
-            
-        </div>
-        <div class="col-md-4">
-            <center>
-                <a class="navbar-brand" href="#">
-                    <img src="{{asset('assets/img/hencorp.jpeg')}}" width="200" height=""  #02163a" class=" d-inline-block align-top" alt="">
-                  </a>
-            </center>
-        </div>
-        <div class="col-md-4">
-
-        </div>
-    
+<nav class="navbar navbar-white bg-azul">
+    <div class="col-md-4">
+    </div>
+    <div class="col-md-4">
+        <center>
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('assets/img/hencorp.jpeg') }}" width="200" height="" #02163a"
+                    class=" d-inline-block align-top" alt="">
+            </a>
+        </center>
+    </div>
+    <div class="col-md-4">
+    </div>
 </nav>
